a2b5c0cf6a8225fb9e45a5c838322d9da3693dbe
Denis Deniben <deniben2109@gmail.com>
2019-01-15 12:42:02 +0200

    One kata is completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        System.out.println(repeatStr(5, "Lolo"));
     }
 
-
+    public static String repeatStr(final int repeat, final String string) {
+        String strings = "";
+        for (int i =0; i < repeat; i++){
+            strings += string;
+        }
+        return strings;
+    }
 }
 class A{
     public static final int B = 1;
