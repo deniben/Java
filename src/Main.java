8eb39215f81461e5daa28830eeecf6dd86da2ec6
Denis Deniben <deniben2109@gmail.com>
2019-01-15 12:00:32 +0200

    Done

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+
+}
+class A{
+    public static final int B = 1;
+    public void method1(){};
+}
+class B extends A {
+
+}
